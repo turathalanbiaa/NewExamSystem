458d6c9d820f886b3e347a5e64c97c44b25df1db
FadhilDev <fadhilabbash@gmail.com>
2019-02-23 14:52:25 +0300

    edit answer

--- app/Http/Controllers/Website/StudentExamController.php
+++ app/Http/Controllers/Website/StudentExamController.php
@@ -16,23 +16,22 @@ class StudentExamController extends Controller
 
     public function exams()
     {
-        try{
-        $student=Student::where('remember_token',Cookie::get('remember_me'))->first();
-        //return response()->json($student->notFinishedExams);
-            return view("Website/exams",compact('student',$student));
-    }
-        catch (\Exception $e) {
+        try {
+            $student = Student::where('remember_token', Cookie::get('remember_me'))->first();
+            //return response()->json($student->notFinishedExams);
+            return view("Website/exams", compact('student', $student));
+        } catch (\Exception $e) {
             return $e->getMessage();
         }
     }
+
     public function finishedExams()
     {
-        try{
-            $student=Student::where('remember_token',Cookie::get('remember_me'))->first();
+        try {
+            $student = Student::where('remember_token', Cookie::get('remember_me'))->first();
             //return response()->json($student->notFinishedExams);
-            return view("Website/finishedExams",compact('student',$student));
-        }
-        catch (\Exception $e) {
+            return view("Website/finishedExams", compact('student', $student));
+        } catch (\Exception $e) {
             return $e->getMessage();
         }
     }
@@ -40,42 +39,43 @@ class StudentExamController extends Controller
 
     public function exam($id)
     {
-        try{
-           $exam= Exam::find($id);
-            return view("Website/exam",compact('exam',$exam));
-    }
-    catch (\Exception $e) {
-        return $e->getMessage();
-    }
+        try {
+            $exam = Exam::find($id);
+            return view("Website/exam", compact('exam', $exam));
+        } catch (\Exception $e) {
+            return $e->getMessage();
+        }
     }
 
     public function store(Request $request)
     {
-        try{
-        $student=Student::where('remember_token',Cookie::get('remember_me'))->first();
-        Answer::where('branch_id',$request->id)->delete();
-
-        $answer = new Answer;
-        $answer->student_id = $student->id;
-        $answer->branch_id = $request->id;
-        $answer->text = $request->val;
-        $answer->time = Carbon::now();
-        $answer->score = 0.0000; //Default 0.00000
-        $answer->re_correct = 0; //Default 0
-        $answer->correction = 0; //Default 0
-        $answer->save();
-
-        return response()->json('ok');
-        }
-        catch (\Exception $e) {
+        try {
+            $student = Student::where('remember_token', Cookie::get('remember_me'))->first();
+            if (Answer::where(['student_id' => $student->id, 'branch_id' => $request->id])->first()) {
+                Answer::where(['student_id' => $student->id, 'branch_id' => $request->id])->update(
+                    ['text' => $request->val]
+                );
+            } else {
+                $answer = new Answer;
+                $answer->student_id = $student->id;
+                $answer->branch_id = $request->id;
+                $answer->text = $request->val;
+                $answer->time = Carbon::now();
+                $answer->score = 0.0000; //Default 0.00000
+                $answer->re_correct = 0; //Default 0
+                $answer->correction = 0; //Default 0
+                $answer->save();
+            }
+            return response()->json('ok');
+        } catch (\Exception $e) {
             return $e->getMessage();
         }
     }
 
     public function finish(Request $request)
     {
-        $examStudent = ExamStudent::where('exam_id',$request->id)->first();
-        $examStudent->state =2;
+        $examStudent = ExamStudent::where('exam_id', $request->id)->first();
+        $examStudent->state = 2;
         $examStudent->save();
         return response()->json($examStudent);
     }
