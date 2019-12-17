50ec2e5f86be7534de7638bb2123a6dfcc3838cf
TurathAlanbiaa <turath.alanbiaa@gmail.com>
2019-12-17 11:44:44 +0300

    update project

--- resources/views/Website/exams.blade.php
+++ resources/views/Website/exams.blade.php
@@ -6,36 +6,32 @@
     <div class="container">
         <div class="row">
             @forelse($studentNotFinishedExams->notFinishedExams->where('state',App\Enums\ExamState::OPEN) as $exam)
-                @if ($exam->type==4)
-                    {{--Second Final Exam--}}
-                    @else
-                    <div class="col-lg-4 col-md-6 col-sm-12 mb-5">
-                        <div class="card shadow h-100">
-                            <div class="view shadow default-color px-3 py-4">
-                                <h5 class="text-center text-white mb-3">
-                                    <a href="javascript:void(0)" class="text-white">{{$exam->title}}</a>
-                                </h5>
-                            </div>
-                            <div class="card-body">
-                                <h5>
-                                    درجة الامتحان :
-                                    {{$exam->real_score}}
-                                </h5>
-                                <p class="card-text text-justify"></p>
-                                <div class="card-body-content-fixed">
-                                    <hr>
-                                    <div class="btn-group w-100">
-                                        <a class="btn  btn-block btn-sm btn-outline-default font-weight-bold  ml-1 mr-0 waves-effect waves-light"
-                                           href="{{URL::to('exam',$exam->id)}}">
-                                            <i class="fa fa-play ml-1"></i>
-                                            <span>بدء</span>
-                                        </a>
-                                    </div>
+                <div class="col-lg-4 col-md-6 col-sm-12 mb-5">
+                    <div class="card shadow h-100">
+                        <div class="view shadow default-color px-3 py-4">
+                            <h5 class="text-center text-white mb-3">
+                                <a href="javascript:void(0)" class="text-white">{{$exam->title}}</a>
+                            </h5>
+                        </div>
+                        <div class="card-body">
+                            <h5>
+                                درجة الامتحان :
+                                {{$exam->real_score}}
+                            </h5>
+                            <p class="card-text text-justify"></p>
+                            <div class="card-body-content-fixed">
+                                <hr>
+                                <div class="btn-group w-100">
+                                    <a class="btn  btn-block btn-sm btn-outline-default font-weight-bold  ml-1 mr-0 waves-effect waves-light"
+                                       href="{{URL::to('exam',$exam->id)}}">
+                                        <i class="fa fa-play ml-1"></i>
+                                        <span>بدء</span>
+                                    </a>
                                 </div>
                             </div>
                         </div>
                     </div>
-                @endif
+                </div>
             @empty
                 <div class="col-12">
                     <div class="alert alert-success py-5 px-3 text-center" role="alert">
